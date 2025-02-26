a242dcaa37aa53e06f3556c4499d4fb23cf1382c
vinoth <mohanrajvinoth2004@gmail.com>
2025-02-26 13:42:08 +0530

    setup .

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import { Outlet } from "react-router-dom";
 import Loading from "./Components/Loading";
 const App = () => {
   const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false); // Hide loader after 2 seconds
