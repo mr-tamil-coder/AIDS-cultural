dbae7d173a7c425c4e987ccb213abdc31e013243
vinoth <mohanrajvinoth2004@gmail.com>
2025-02-27 22:01:37 +0530

    updated loader

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false); // Hide loader after 2 seconds
-    }, 5000);
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
